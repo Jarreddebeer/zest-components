fdf0f7bb628e80a446e6bd297d74322aec9fb4ab
Jarred de Beer <jarr2000@gmail.com>
2013-01-18 13:59:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+define(['zest', 'jquery', 'com!app/FlexSlider/flexslider'], function($z, $, FlexSlider) {
+  $z.render(FlexSlider, {
+    animation: 'slide',
+    slideshowSpeed: 2500,    
+  }, document.body);
+});
